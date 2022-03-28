6ae34a65ae57e819d2617e901c103aab62a3910d
jacelyntayxn <94168656+jacelyntayxn@users.noreply.github.com>
2022-03-28 15:33:44 +0800

    Update Question2.py

--- Question2.py
+++ Question2.py
@@ -5,15 +5,19 @@ class clockTime:
         self.minutes = minutes
         self.hours = hours
 
+    #Set hours
     def setHours(self, hours):
         self.hours = hours
 
+    #Set minutes
     def setMinutes(self, minutes):
         self.minutes = minutes
     
+    #Set seconds
     def setSeconds(self, seconds):
         self.seconds = seconds
     
+    #show time
     def showTime(self):
         print("The time is: {}:{}:{}".format(self.hours,self.minutes,self.seconds))
 
@@ -22,5 +26,8 @@ hour = input("Please enter hour:")
 minute = input("Please enter minute:")
 second = input("Please enter second: ")
 
+#create a variable to pass in the parameters needed in the class clockTime
 time = clockTime(hour, minute, second)
+
+#Print the time 
 time.showTime()
